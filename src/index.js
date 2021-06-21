f2763c00600e0ed29db7bf53100fec86af4715f3
AndreaOpazo <andree.2593@gmail.com>
2021-06-21 20:25:30 -0300

    elimino strict mode

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
-import React, { StrictMode } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+ReactDOM.render(<App />, document.getElementById('root'));
