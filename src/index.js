0da4c7ec479d8fcfa783af22ddd70e032a7748e9
AndreaOpazo <andree.2593@gmail.com>
2021-06-24 19:03:31 -0300

    modifico fontfamily

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '@fontsource/oswald';
 
 ReactDOM.render(<App />, document.getElementById('root'));
