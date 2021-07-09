d6a97aa78b37fb8a12b5fdaf2b934dee53fc2c42
AndreaOpazo <andree.2593@gmail.com>
2021-07-08 21:28:11 -0300

    agrego configuraciones para la db

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDt2xNt4izSyKgEVvMzgt_kjzix8TL64YA",
+  authDomain: "asi-como-eres.firebaseapp.com",
+  projectId: "asi-como-eres",
+  storageBucket: "asi-como-eres.appspot.com",
+  messagingSenderId: "607068651975",
+  appId: "1:607068651975:web:9edd48f840990121d1d9e2"
+};
+
+export default firebaseConfig;
