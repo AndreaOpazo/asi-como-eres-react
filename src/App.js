333ff9da48aeec4bd3b79577951727b5c5c35ccc
AndreaOpazo <andree.2593@gmail.com>
2021-07-13 14:24:10 -0300

    desinstalo semantic ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import 'semantic-ui-css/semantic.min.css';
 import Header from './components/Header/Header';
 import Home from './views/Home/Home';
 import ProductDetailContainer from './views/ProductDetailContainer/ProductDetailContainer';
